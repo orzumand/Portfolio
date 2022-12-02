3556206273eaa5c6eeccc3046aa6870c701002e8
orzumand <hayotbekyoldashev0@gmail.com>
2022-12-02 17:41:42 +0500

    about

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,7 +4,31 @@ import { Container } from "./style";
 const About = () => {
   return (
     <Container>
-      <h1>About</h1>
+      <h1>This is my About page </h1>
+      <h6>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, quae?
+        Eligendi eius aliquam illum, nulla excepturi similique dolores facilis
+        voluptate numquam distinctio officiis et repellat in esse beatae aperiam
+        exercitationem doloribus quaerat quidem delectus facere pariatur iste
+        incidunt? Dolor libero reprehenderit repudiandae beatae, dolore
+        voluptate repellat nam quis dolorem id iste deleniti mollitia minus.
+        Sunt, quas debitis earum natus rem porro nulla animi assumenda
+        architecto voluptate sint voluptatum possimus inventore? Hic, esse eos
+        iste itaque repellendus in neque sequi magni amet sapiente et! Ea unde
+        vero vel suscipit voluptas? Magnam quam tenetur assumenda blanditiis
+        vero velit alias sapiente in itaque nulla quos unde, accusamus
+        reprehenderit rem amet quasi cupiditate fugiat doloremque sunt sequi
+        molestiae laboriosam praesentium. Expedita consequatur voluptas tempore
+        delectus sapiente voluptatum repudiandae dolore nobis a dolorem enim
+        repellendus suscipit eligendi excepturi at autem deleniti quaerat quasi
+        minus, tenetur iure et iste assumenda. Natus labore doloribus excepturi
+        distinctio aliquam dicta placeat tenetur animi, facilis pariatur ab
+        obcaecati praesentium illo porro qui a sint. Est molestiae, odio
+        cupiditate minus enim eius, esse debitis, repudiandae quod saepe
+        perferendis sequi at? Modi consequatur officia nisi amet qui! Neque
+        corrupti dolor accusantium alias pariatur, quaerat error, corporis
+        inventore nulla, maiores dignissimos iure perferendis.
+      </h6>
     </Container>
   );
 };
