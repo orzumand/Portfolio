8111b91e2104fb166bda9a95d8c406b258d963f1
orzumand <hayotbekyoldashev0@gmail.com>
2022-12-03 18:01:26 +0500

    dynamic old

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,21 +4,37 @@ import { useState } from "react";
 import { Container, Text } from "./style";
 
 const About = () => {
-  const [date, setDate] = useState("");
+  const [old, setOld] = useState("");
+  const newYears = "May 17, 2000 2:59:0";
+  const newYearDate = new Date(newYears);
+  const currentDate = new Date();
+
+  const TotalSeconds = (currentDate - newYearDate) / 1000;
+  const year = Math.floor(TotalSeconds / 31536000);
+  const days = Math.floor(TotalSeconds / 31536000 / 24);
+  const hours = Math.floor(TotalSeconds / 3600) % 24;
+  const minutes = Math.floor(TotalSeconds / 60) % 60;
+  const seconds = Math.floor(TotalSeconds) % 60;
+
+  useEffect(() => {
+    const OldID = setInterval(
+      () => setOld(`${year}/${days}/${hours}/${minutes}/${seconds}`),
+      1000
+    );
+    return function cleeanup() {
+      clearInterval(OldID);
+    };
+  });
+
+  console.log(year);
 
   const d = new Date();
   const h = d.getFullYear();
-  const m = d.getUTCDay();
-  const s = d.getDay();
-  const Old = h - 2000;
-  useEffect(() => {
-    const time = setInterval(() => setDate(`${m}/${s}/${h}`), 1000);
-    console.log(date);
+  const m = d.getDate();
+  const s = d.getMonth();
 
-    return function cleanup() {
-      clearInterval(time);
-    };
-  });
+  const date = `${m}/${s}/${h}`;
+
   console.log(d);
   return (
     <Container>
@@ -26,9 +42,11 @@ const About = () => {
       <Text>
         Hi, my full name is ''
         <div className="yellow"> Hayotbek Yo'ldashev</div>'', I was born in
-        Kashkadarya region, O'zbekistan in 2000. I'm {Old} years old.
+        Kashkadarya region, O'zbekistan in 2000. I'm {old} years old.
       </Text>
-      <h2>{date}</h2>
+      <Text>
+        Tooday : <div className="yellow"> {date}</div>
+      </Text>
     </Container>
   );
 };
