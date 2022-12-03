a6757fb722480a576d364cba2a6a2939ba6a3084
orzumand <hayotbekyoldashev0@gmail.com>
2022-12-03 14:31:03 +0500

    About page is building

--- src/components/About/style.js
+++ src/components/About/style.js
@@ -1,13 +1,22 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  color: var(--colorPrimary);
   width: 85%;
+  padding-top: 20px;
   height: 100vh;
   display: flex;
-  color: #ffff;
-  justify-content: center;
-  background-color: #ff6000;
+  flex-direction: column;
+  color: #219ebc;
+  align-items: center;
 `;
 
-export { Container };
+const Text = styled.div`
+  font-size: 20px;
+  .yellow {
+    color: #ffb703;
+  }
+  /* border: 2px solid red; */
+  display: flex;
+`;
+
+export { Container, Text };
